df727303f52c8f6290fa4a7df905f62614a1421c
00giemensch <75619006+00giemensch@users.noreply.github.com>
2025-08-14 23:00:10 +0400

    add RecipeDetailViewController

---
+++ BestRecipes/BestRecipes/Modules/Recipe detail/RecipeDetailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  RecipeDetailViewController.swift
+//  BestRecipes
+//
+//  Created by Ilnur on 14.08.2025.
+//
+
+import UIKit
+
+class RecipeDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
