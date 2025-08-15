ad06da101490431af9957990bba956c2ff36b31a
00giemensch <75619006+00giemensch@users.noreply.github.com>
2025-08-15 23:54:51 +0400

    new changes in recipe detail

--- BestRecipes/BestRecipes/Modules/Recipe detail/RecipeDetailViewController.swift
+++ BestRecipes/BestRecipes/Modules/Recipe detail/RecipeDetailViewController.swift
@@ -22,6 +22,7 @@ class RecipeDetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
+//        view.backgroundColor = .white
         title = recipe.title
     }
 }
