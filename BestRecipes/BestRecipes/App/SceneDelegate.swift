bd79d733765b135b88c025d7996709809d5f3867
00giemensch <75619006+00giemensch@users.noreply.github.com>
2025-08-18 20:44:33 +0400

    solve sceneDelegate

--- BestRecipes/BestRecipes/App/SceneDelegate.swift
+++ BestRecipes/BestRecipes/App/SceneDelegate.swift
@@ -14,9 +14,9 @@ final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-        //        window?.rootViewController = SearchViewController()
-                window?.rootViewController = UINavigationController(rootViewController: HomeViewController())
-                window?.makeKeyAndVisible()
+//        window?.rootViewController = SearchViewController()
+        window?.rootViewController = UINavigationController(rootViewController: HomeViewController())
+        window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
